895d367e52e0e32c984139a1e679b95ae213d441
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-23 13:43:26 -0300

    fix rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/create'
-  get 'restaurants/new'
-  get 'restaurants/edit'
-  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurant, except: [:update, :destroy]
+  resources :restaurants, except: [:update, :destroy]
 end
